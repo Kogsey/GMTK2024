fef0f79b78b8a98dfd499adc9d5f3e864e147a51
MelBolt <68885800+MelBolt@users.noreply.github.com>
2024-08-20 19:09:53 +0800

    update map paths

--- Assets/Scripts/Managers/Map/PathHelper.cs
+++ Assets/Scripts/Managers/Map/PathHelper.cs
@@ -11,7 +11,7 @@ public class PathHelper : MonoBehaviour
 		Vector2 endPosition = Node2.transform.position;
 		Vector2 startPosition = Node1.transform.position;
 		float angle = Mathf.Rad2Deg * Mathf.Atan2(endPosition.y - startPosition.y, endPosition.x - startPosition.x);
-		SpriteRenderer.size = new Vector2(Vector2.Distance(startPosition, endPosition), 2.5f);
+		SpriteRenderer.size = new Vector2(Vector2.Distance(startPosition, endPosition), 1.25f);
 
 		Vector2 centrePos = startPosition + (endPosition - startPosition) / 2;
 		transform.SetPositionAndRotation(centrePos, Quaternion.AngleAxis(angle, Vector3.forward));
